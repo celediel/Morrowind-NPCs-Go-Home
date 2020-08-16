e0ed82a9d2bc647740c1e4038821c780cda65451
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2020-08-15 17:17:44 -0700

    mostly use this for debugging with livecoding but maybe it'll be useful

--- MWSE/mods/celediel/NPCsGoHome/interop.lua
+++ MWSE/mods/celediel/NPCsGoHome/interop.lua
@@ -1,21 +1,18 @@
 local this = {}
 
+-- access to the NPCs that have homes or have been assigned a public house
 local homedNPCs = {}
-
 this.setHomedNPCTable = function(t) homedNPCs = t end
-
 this.getHomedNPCTable = function() return homedNPCs end
 
-local inns = {}
+-- access to any cells that have been marked public
+local pubs = {}
+this.setPublicHouseTable = function(t) pubs = t end
+this.getPublicHouseTable = function() return pubs end
 
-this.setInnTable = function(t) inns = t end
-
-this.getInnTable = function() return inns end
-
-local movedNPCs = {}
-
-this.setMovedNPCsTable = function(t) movedNPCs = t end
-
-this.getMovedNPCsTable = function() return movedNPCs end
+-- access to NPCs that have been moved
+local moved = {}
+this.setMovedNPCTable = function(t) moved = t end
+this.getMovedNPCTable = function() return moved end
 
 return this
